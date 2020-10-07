fedf9837bade751610e71965fa98bb341f966d71
smachball21 <69909407+smachball21@users.noreply.github.com>
2020-10-07 16:08:57 +0200

    new master

---
+++ database/migrations/2020_10_07_063327_create_usersgroup_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersgroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usersgroup', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->unsignedBigInteger('userID')->index('username');
+            $table->integer('groupID')->nullable()->default(1)->index('groupe');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usersgroup');
+    }
+}
